fa3f2ffe643f7977238b6fb442443ec08c3cdbdc
Jinki Jung <your.jinki.jung@gmail.com>
2020-01-30 23:01:26 +0100

    store json scenario as file

--- js/editor/tascEditor.js
+++ js/editor/tascEditor.js
@@ -83,10 +83,28 @@ function download(data, filename, type) {
     }
 }
 
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
+
 function saveScenario(){
     var scenario = exportTascToJSON(getID('scenario'), 'test', 'This is test',outputTascData);
     //var scenario = new Scenario(getID('scenario'), 'test', 'This is test', terminusData, actionData, conditionData, instructionData, tascData);//{ id: ID(), name:"testing", terminuses: terminusData, actions: actionData, conditions: conditionData, instructions: instructionData, scenario:tascData };
-    console.log(scenario);
+    download(JSON.stringify(scenario), scenario.name, 'application/json');
 }
 
 function loadScenario(){
